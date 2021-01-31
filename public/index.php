afe4da35eb62e3698fbab1c6c9f5de1dacc6eb53
Rik Meijer <rik@rikmeijer.nl>
2021-01-31 21:48:32 +0100

    renew interface

--- public/index.php
+++ public/index.php
@@ -16,24 +16,24 @@
 </head>
 <body>
 <main role="main" class="container vertical-center">
-<header class="text-right">
-    <h1 style="font-family: 'Bungee', cursive;">Rik Meijer</h1>
-    <h2 style="font-family: 'Bungee Shade', cursive;">mastering the art of living by mentoring others</h2>
+<header class="block" >
+    <h1>Rik Meijer</h1>
+    <h2>mastering the art of living by mentoring others</h2>
 </header>
+
+    <footer class="block">
+        <div class="container text-center">
+            <small class="text-muted">Copyright &copy; Rik Meijer <?=date("Y");?></small>
+            <ul>
+                <li><a href="https://github.com/rikmeijer" rel="me"><img src="img/GitHub-Mark-32px.png" height="16" width="16"/> </a></li>
+                <li><a href="mailto:hello@rikmeijer.nl" rel="me">hello@rikmeijer.nl</a></li>
+            </ul>
+            <link rel="pgpkey" href="/key.gpg">
+        </div>
+
+    </footer>
 </main>
 
-
-    <footer class="footer">
-        <div class="container text-center">
-        <small class="text-muted">Copyright &copy; <a href="https://github.com/rikmeijer/">Rik Meijer</a> <script>document.write(new Date().getFullYear())</script></small>
-            <ul>
-              <li><a href="https://github.com/rikmeijer" rel="me">Github</a></li>
-              <li><a href="mailto:hello@rikmeijer.nl" rel="me">hello@rikmeijer.nl</a></li>
-            </ul>
-        <link rel="pgpkey" href="/key.gpg">
-        </div>
-        
-    </footer>s
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
