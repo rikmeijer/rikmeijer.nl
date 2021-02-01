685b94bb9aee7398be8b565cbcd1552cda4474fe
Rik Meijer <rik@rikmeijer.nl>
2021-02-01 14:26:56 +0100

    use path util to join paths os independently

--- config.defaults.php
+++ config.defaults.php
@@ -1,7 +1,8 @@
 <?php declare(strict_types=1);
-return [
-    'twig' => [ // must be same as basename of resource loader
-        'templates' => __DIR__ . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'twig',
-        'cache' => __DIR__ . DIRECTORY_SEPARATOR . 'storage' . DIRECTORY_SEPARATOR . 'twig'
-    ]
-];
+
+use Webmozart\PathUtil\Path;
+
+return ['twig' => [ // must be same as basename of resource loader
+    'templates' => Path::join(__DIR__, 'resources', 'twig'),
+    'cache' => Path::join(__DIR__, 'storage', 'twig')
+]];
