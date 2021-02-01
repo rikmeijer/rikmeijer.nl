544e0b0d222a3fbc5b0e69064ad18a7d19f17b82
Rik Meijer <rik@rikmeijer.nl>
2021-02-01 21:40:21 +0100

    add nice picture of meself

--- bootstrap/twig.php
+++ bootstrap/twig.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Twig\TwigFunction;
+
 return function (array $configuration): Environment {
     $loader = new FilesystemLoader($configuration['templates']);
 
@@ -10,5 +12,16 @@ return function (array $configuration): Environment {
         $options['cache'] = $configuration['cache'];
     }
 
-    return new Environment($loader, $options);
+    $twig = new Environment($loader, $options);
+
+    $twig->addFunction(new TwigFunction('img', function (string $path, float $size = 1) use ($configuration) {
+        if ($size < 1) {
+            $width = (100*$size) . '%';
+        } else {
+            $width = "".$size;
+        }
+        return '<img src="' . htmlentities($configuration['images'] . $path) . '" width="' . htmlentities($width) . '" />';
+    }, ['is_safe' => ['html']]));
+
+    return $twig;
 };
--- config.defaults.php
+++ config.defaults.php
@@ -4,5 +4,6 @@ use Webmozart\PathUtil\Path;
 
 return ['twig' => [ // must be same as basename of resource loader
     'templates' => Path::join(__DIR__, 'resources', 'twig'),
+    'images' => "/img/",
     'cache' => Path::join(__DIR__, 'storage', 'twig')
 ]];
