f05684bb6ae1b523ba300bb470fa59f62616ce44
Rik Meijer <rik@rikmeijer.nl>
2021-02-08 13:27:28 +0100

    configure php-binary to use in self-updater

--- bootstrap/selfupdater.php
+++ bootstrap/selfupdater.php
@@ -5,22 +5,22 @@ use Webmozart\PathUtil\Path;
 
 return function (array $configuration): Closure {
     return function(string $workingDir) use ($configuration) {
-
         chdir($workingDir);
+        $composerCommand = $configuration['php-binary'] . ' composer.phar install';
         switch ($configuration['stage']) {
             case 'production':
-                exec('composer install --no-dev');
+                exec( $composerCommand . '  --no-dev');
                 break;
 
             case 'testing':
             case 'development':
-                exec('composer install');
+                exec($composerCommand);
                 break;
         }
 
         $createDirectory = $this->resource('selfupdater/storage')(Path::join($workingDir, 'storage'));
         array_map($createDirectory, $configuration['storages']);
 
-        exec('php build.php');
+        exec($configuration['php-binary'] . ' build.php');
     };
 };
