1a304c30667db1535e6dc4dfb029ba5f15cb5742
Rik Meijer <rik@rikmeijer.nl>
2021-02-08 21:35:48 +0100

    missing newline

--- bootstrap/selfupdater/build/posts.php
+++ bootstrap/selfupdater/build/posts.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 return function (array $configuration): Closure {
     $buildPost = $this->resource('selfupdater/build/post');
     return function(string $to) use ($configuration, $buildPost) : array {
-        print 'Opening blogs in ' . $configuration['from'];
+        print PHP_EOL . 'Opening blogs in ' . $configuration['from'];
         $posts = [];
         foreach (glob($configuration['from'] . DIRECTORY_SEPARATOR . '*.md') as $post) {
             print PHP_EOL . 'Parsing `' . basename($post) . '`...';
