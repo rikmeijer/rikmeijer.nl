4b3a64075f467a279e96bd0738b327263d0d0087
Rik Meijer <rik@rikmeijer.nl>
2021-02-17 22:17:38 +0100

    upgraded to Bootstrap 1.1.4

--- bootstrap/selfupdater/build.php
+++ bootstrap/selfupdater/build.php
@@ -1,15 +1,12 @@
 <?php /** @noinspection PhpUndefinedVariableInspection */
 declare(strict_types=1);
 
+use rikmeijer\Bootstrap\Configuration;
 use Webmozart\PathUtil\Path;
 
 $configuration = $validate([
-    'to' => static function(?string $path, array $context) {
-        return $path ?? Path::join($context['configuration-path'], 'public');
-    },
-    'custom-scss' => static function(?string $path, array $context) {
-        return $path ?? Path::join($context['configuration-path'], 'resources', 'scss', 'custom.scss');
-    },
+    'to' => Configuration::path('public'),
+    'custom-scss' => Configuration::path('resources','scss','custom.scss'),
     'sass-binary' => static function(?string $path) {
         if (file_exists($path) === false) {
             trigger_error('sass-binary does not exist', E_USER_ERROR);
