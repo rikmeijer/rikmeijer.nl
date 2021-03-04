494b96d36d68a8bedb5d63e9c5b0ff73fdec1c08
Rik Meijer <rik@rikmeijer.nl>
2021-03-04 22:33:43 +0100

    require dsn, username and password in sabre/pdo config

--- bootstrap/sabre/pdo.php
+++ bootstrap/sabre/pdo.php
@@ -4,9 +4,14 @@ declare(strict_types=1);
 namespace rikmeijer\nl\sabre;
 
 use PDO as _PDO;
+use function rikmeijer\Bootstrap\configuration\string;
 
 return pdo\configure(static function (array $configuration) : _PDO {
     $pdo = new _PDO($configuration['dsn'], $configuration['username'], $configuration['password']);
     $pdo->setAttribute(_PDO::ATTR_ERRMODE, _PDO::ERRMODE_EXCEPTION);
     return $pdo;
-}, []);
+}, [
+    'dsn' => string(null),
+    'username' => string(null),
+    'password' => string(null),
+]);
