f963ae4e6ce0198bc091e977fe16eea749ebe719
Rik Meijer <rik@rikmeijer.nl>
2021-04-06 16:07:06 +0200

    use binary type for sass generator

--- bootstrap/selfupdater.php
+++ bootstrap/selfupdater.php
@@ -4,9 +4,12 @@ declare(strict_types=1);
 namespace rikmeijer\nl;
 
 use Webmozart\PathUtil\Path;
+use Webmozart\PathUtil\Path as WMPath;
 use function rikmeijer\Bootstrap\configure;
 use function rikmeijer\Bootstrap\types\arr;
-use function rikmeijer\nl\selfupdater\build;
+use function rikmeijer\Bootstrap\types\binary;
+use function rikmeijer\Bootstrap\types\path;
+use function rikmeijer\nl\selfupdater\build\site;
 use function rikmeijer\nl\selfupdater\directory;
 
 return configure(static function (array $configuration, string $workingDir) : void {
@@ -15,7 +18,17 @@ return configure(static function (array $configuration, string $workingDir) : vo
     $createStorage = directory(Path::join($workingDir, 'storage'));
     array_map($createStorage, $configuration['storages']);
 
-    build();
+    print PHP_EOL . 'Generating site...';
+    site($configuration['to']);
+    print 'done' . PHP_EOL;
+
+    $customCSSFile = WMPath::join($configuration['to'], 'css', 'custom.css');
+    directory(dirname($customCSSFile));
+    $configuration['sass-binary']('Generating CSS...', $configuration['custom-scss'], $customCSSFile);
+    echo 'done';
 }, [
     'storages' => arr(['twig', 'sabre', 'sabre/data', 'sabre/files', 'sabre/files/blog']),
+    'to' => path('public'),
+    'custom-scss' => path('resources/scss/custom.scss'),
+    'sass-binary' => binary()
 ]);
