91faafde293d893386285ee0ff264fb5a98b36c5
Rik Meijer <rik@rikmeijer.nl>
2021-04-06 20:18:43 +0200

    set BOOTSTRAP_CONFIGURATION_PATH env var

--- public/dav.php
+++ public/dav.php
@@ -3,5 +3,5 @@
 use function rikmeijer\nl\sabre;
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor/autoload.php';
-require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
+putenv("BOOTSTRAP_CONFIGURATION_PATH=" . dirname(__DIR__));
 sabre();
